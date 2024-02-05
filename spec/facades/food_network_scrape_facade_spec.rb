48798e70c443c952ea7749826e44f45163dd245b
Nathan Trautenberg <Natester123@mac.com>
2024-02-05 15:48:47 -0700

    made facade and poro tests for foodnetwork web scraping.

---
+++ spec/facades/food_network_scrape_facade_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe FoodNetworkScrapeFacade do
+  describe 'facade' do
+    it 'creates web scrape poro from food network info' do
+      VCR.use_cassette('foodnetwork_search') do
+        scrape = FoodNetworkScrapeFacade.new.recipe_details('https://www.foodnetwork.com/recipes/food-network-kitchen/the-perfect-grilled-cheese-3636831')
+        
+        expect(scrape).to be_a(FoodNetworkPoro)
+        expect(scrape.name).to eq('The Perfect Grilled Cheese')
+        expect(scrape.ingredients).to be_an(Array)
+        expect(scrape.ingredients.count).to eq(3)
+        expect(scrape.instructions).to be_a(String)
+      end
+    end
+  end
+end
