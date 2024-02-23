23e72c3472f4e47737024cb128e3f655f881dd19
Allan Evans <oneblankpage27@gmail.com>
2024-02-23 14:20:41 -0700

    Added caching for specific recipes too, haven't written tests due to the default test enviorment setting the caching to nil.  However testing the endpoints in Postman they work like expected

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -14,7 +14,6 @@ class Api::V1::SearchesController < ApplicationController
 
   private
   def food_search(search_term)
-    require 'pry';binding.pry
     if Rails.cache.exist?(search_term)
       render json: MealSerializer.new(Rails.cache.read(search_term)), status: 200
     else
@@ -29,26 +28,32 @@ class Api::V1::SearchesController < ApplicationController
   end
 
   def scrape_web(website)
-    if WebScrapeService.new.conn(website).status == 200
-      scrape_from_sites(website)
+    if Rails.cache.exist?(website)
+      render json: WebScrapeSerializer.recipes(Rails.cache.read(website)), status: 200
     else
-      render json: { errors: [title: "Please provide a correct website link", status: "400"]}, status: :bad_request
+      if WebScrapeService.new.conn(website).status == 200
+        scrape_from_sites(website)
+      else
+        render json: { errors: [title: "Please provide a correct website link", status: "400"]}, status: :bad_request
+      end
     end
   end
 
   def scrape_from_sites(website)
-    if website.include?('awickedwhisk.com')
-      render json: WebScrapeSerializer.recipes(WebScrapeFacade.new.recipe_details(website)), status: :ok
-    elsif website.include?('allrecipes.com')
-      facade = AllRecipesScrapeFacade.new
+    facade = ""
+    if website.include?('awickedwhisk.com') || website.include?('allrecipes.com') || website.include?('foodnetwork.com') || website.include?('tasteofhome.com')
+      if website.include?('awickedwhisk.com')
+        facade = WebScrapeFacade.new
+      elsif website.include?('allrecipes.com')
+        facade = AllRecipesScrapeFacade.new
+      elsif website.include?('foodnetwork.com')
+        facade = FoodNetworkScrapeFacade.new
+      elsif website.include?('tasteofhome.com')
+        facade = TohScrapeFacade.new
+      end
       recipe_results = facade.recipe_details(website)
-      render json: WebScrapeSerializer.recipes(recipe_results), status: 200
-    elsif website.include?('foodnetwork.com')
-      facade = FoodNetworkScrapeFacade.new
-      recipe_results = facade.recipe_details(website)
-      render json: WebScrapeSerializer.recipes(recipe_results), status: 200
-    elsif website.include?('tasteofhome.com')
-      render json: WebScrapeSerializer.recipes(TohScrapeFacade.new.recipe_details(website)), status: :ok
+      Rails.cache.write(website, recipe_results)
+      render json: WebScrapeSerializer.recipes(recipe_results), status: :ok
     else
       render json: { errors: [title: "Please provide a link from websites: a wicked whisk, all recipes, food network, taste of home", status: "400"]}, status: :bad_request
     end
